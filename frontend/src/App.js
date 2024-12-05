1616156cb0c73b761bb7ec5713cbecd9eedc01ce
franzfelini <arnautovic.feda@gmail.com>
2024-12-05 13:26:00 +0100

    Device Checks Vol.15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,6 +65,7 @@ function App() {
     sendDeviceInfoToBackend();
   }, []);
 
+  ///nanana test nesto
   const handleFuelChange = (selectedOption) => {
     setSelectedFuelType(selectedOption);
   };
